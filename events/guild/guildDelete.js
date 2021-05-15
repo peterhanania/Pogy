f624d836d7469a32465dd2c35df73b4c304da01f
peterhanania <72696414+peterhanania@users.noreply.github.com>
2021-05-15 14:17:18 +0300

    Update guildDelete.js

--- events/guild/guildDelete.js
+++ events/guild/guildDelete.js
@@ -3,9 +3,10 @@ const Discord = require('discord.js');
 const logger = require('../../utils/logger');
 const Guild = require('../../database/schemas/Guild');
 const metrics = require('datadog-metrics');
-const Logging = require('../../database/schemas/logging')
-const webhookClient = new Discord.WebhookClient('', '');
-const welcomeClient = new Discord.WebhookClient('','');
+const Logging = require('../../database/schemas/logging');
+const config = require('../../config.json');
+const webhookClient = new Discord.WebhookClient(config.webhook_id, config.webhook_url);
+const welcomeClient = new Discord.WebhookClient(config.webhook_id, config.webhook_url);
 module.exports = class extends Event {
 
   async run(guild) {
