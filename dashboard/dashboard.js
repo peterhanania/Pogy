debd05ae18d2ec2b2ac0be8f4e701fed58270ade
peterhanania <72696414+peterhanania@users.noreply.github.com>
2021-05-15 14:29:25 +0300

    Update dashboard.js

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -5,13 +5,14 @@ let uniqid = require('uniqid');
 const cooldownNickname = new Set();
 const express = require("express");
 const passport = require("passport");
+const jsonconfig = require('../config.json');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const Strategy = require("passport-discord").Strategy;
-const premiumWeb = new Discord.WebhookClient('', '');
+const premiumWeb = new Discord.WebhookClient(jsonconfig.webhook_id, jsonconfig.webhook_url);
 const config = require("../config");
 const ejs = require("ejs");
-const jsonconfig = require('../config.json');
+
 
 const ShortUrl = require('../models/ShortUrl.js');
 const { mem, cpu, os } = require('node-os-utils');
@@ -41,7 +42,7 @@ const fs = require('fs');
 const Application = require("../models/application/application.js");
 const customCommand = require('../database/schemas/customCommand.js');
 //dont touch here
-const Hook =  new WebhookClient('', '');
+const Hook = new WebhookClient(jsonconfig.webhook_id, jsonconfig.webhook_url);
 //
 
 let rgx = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
