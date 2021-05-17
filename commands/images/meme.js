1314dda8bbc5312aef8c27a74f374c170e83c5a7
peterhanania <72696414+peterhanania@users.noreply.github.com>
2021-05-17 10:23:50 +0300

    Update meme.js

--- commands/images/meme.js
+++ commands/images/meme.js
@@ -1,5 +1,7 @@
 const Command = require('../../structures/Command');
 const fetch = require('node-fetch');
+const Discord = require('discord.js');
+const got = require('got');
 
 module.exports = class extends Command {
     constructor(...args) {
@@ -7,28 +9,28 @@ module.exports = class extends Command {
         name: 'meme',
         description: 'Generate some memes!',
         category: 'Images',
-        cooldown: 2
+        cooldown: 5
       });
     }
 
     async run(message) {
       try {
-        const data = await fetch(`https://meme-api.herokuapp.com/gimme`)
-        .then(res => res.json())
-      
-        const { title, postLink, url, subreddit } = data
-
-        message.channel.send({
-          embed: {
-            color: "BLURPLE",
-            title: `${title}`,
-            url: `${postLink}`,
-            image: {
-              url: url
-            },
-            footer: { text: `/reddit/${subreddit}` }
-          }
-        });
+        const embed = new Discord.MessageEmbed()
+       got('https://www.reddit.com/r/memes/random/.json').then(response => {
+        let content = JSON.parse(response.body);
+        let permalink = content[0].data.children[0].data.permalink;
+        let memeUrl = `https://reddit.com${permalink}`;
+        let memeImage = content[0].data.children[0].data.url;
+        let memeTitle = content[0].data.children[0].data.title;
+        let memeUpvotes = content[0].data.children[0].data.ups;
+        let memeNumComments = content[0].data.children[0].data.num_comments;
+        embed.setTitle(`${memeTitle}`);
+        embed.setURL(`${memeUrl}`)
+        embed.setColor('RANDOM')
+        embed.setImage(memeImage);
+        embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
+        message.channel.send(embed);
+       });
       } catch(error) {
         this.client.emit("apiError", error, message);
       }
