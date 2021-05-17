4e0a80615b1c611c47e16ca150f33547017cc301
peterhanania <72696414+peterhanania@users.noreply.github.com>
2021-05-17 10:07:56 +0300

    Update blacklist.js

--- commands/owner/blacklist.js
+++ commands/owner/blacklist.js
@@ -67,7 +67,6 @@ member =  match ? message.mentions.members.first() || message.guild.members.fetc
         const embed = new MessageEmbed()
           .setColor('BLURPLE')
           .setTitle(`Blacklist Report`)
-          .setURL('https://docs.google.com/forms/d/e/1FAIpQLSedBS6JYEqxzXGcs1T9vb7dugZiBxftVrY3CFfk-aP1rFnnrw/viewform?usp=sf_link')
           .addField('Status', 'Added to the blacklist.')
           .addField('User', `${member.user.tag} (${member.id})`)
           .addField('Responsible', `${message.author} (${message.author.id})`)
