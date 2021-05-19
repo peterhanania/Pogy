348b9d87e38b93e3440ad9a2de54131f8f73692a
peterhanania <72696414+peterhanania@users.noreply.github.com>
2021-05-19 13:50:14 +0300

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -50,7 +50,24 @@ guildId: message.guild.id
 const language = require(`../../data/language/${guildDB.language}.json`)
 
 
-const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+let member = message.mentions.members.last();
+ 
+     
+     if(!member) {
+
+      try {
+
+       member = await message.guild.members.fetch(args[0])
+
+     } catch {
+
+member = message.member;
+
+     }
+        
+        
+
+       }
 
 if (!member)
 return message.channel.send( new MessageEmbed()
