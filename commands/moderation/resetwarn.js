76b2978bf9e4154c02032a0689255d6919f89181
peterhanania <72696414+peterhanania@users.noreply.github.com>
2021-05-19 14:14:26 +0300

    Update resetwarn.js

--- commands/moderation/resetwarn.js
+++ commands/moderation/resetwarn.js
@@ -51,7 +51,7 @@ guildId: message.guild.id
 const language = require(`../../data/language/${guildDB.language}.json`)
   const logging = await Logging.findOne({ guildId: message.guild.id })
 
-const mentionedMember = message.mentions.members.first()
+const mentionedMember = message.mentions.members.last()
 || message.guild.members.cache.get(args[0])
 
 
