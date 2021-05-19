629c009bd05be7767a64e845042d2230843295a2
peterhanania <72696414+peterhanania@users.noreply.github.com>
2021-05-19 13:49:33 +0300

    Update addrole.js

--- commands/moderation/addrole.js
+++ commands/moderation/addrole.js
@@ -55,9 +55,24 @@ guildId: message.guild.id
 const language = require(`../../data/language/${guildDB.language}.json`)
 
 
+let member = message.mentions.members.last();
+ 
+     
+     if(!member) {
 
-       const member = message.mentions.members.first()
-        || message.guild.members.cache.get(args[0])
+      try {
+
+       member = await message.guild.members.fetch(args[0])
+
+     } catch {
+
+member = message.member;
+
+     }
+        
+        
+
+       }
 
     if (!member)
       return message.channel.send( new MessageEmbed()
