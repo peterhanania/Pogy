1fd037d5f6f9333a871a240aa9c792f53cddaaf5
peterhanania <72696414+peterhanania@users.noreply.github.com>
2021-05-24 13:00:37 +0300

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -53,24 +53,11 @@ guildId: message.guild.id
 const language = require(`../../data/language/${guildDB.language}.json`)
 
 
-let member = message.mentions.members.last();
- 
-     
-     if(!member) {
-
-      try {
-
-       member = await message.guild.members.fetch(args[0])
-
-     } catch {
-
-member = message.member;
-
-     }
+let member = message.mentions.members.last() || message.guild.members.cache.get(args[0]);
         
         
 
-       }
+
 
 if (!member) {
   await client.users.fetch(args[0])
