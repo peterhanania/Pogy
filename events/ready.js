94b959225c764c27fb566e46a6dfc082c23f1799
eNtee-bots <71556073+eNtee-bots@users.noreply.github.com>
2021-07-04 18:06:31 +0200

    sharding and so

--- events/ready.js
+++ events/ready.js
@@ -11,35 +11,8 @@ const maintenance = await Maintenance.findOne({
 
 if(maintenance && maintenance.toggle == "true"){
 
-  
-
-    this.client.user.setPresence({ status: 'dnd' });
-    this.client.user.setActivity('Under Maintenance')
-
-  
-
 logger.info(`✅ loaded Maintenance Mode `, { label: 'Status' })
 } else {
-    const activities = [
-      { name: 'p!help | pogy.xyz', type: 'WATCHING' }, 
-      { name: '@pogy', type: 'WATCHING' }
-    ];
-  
-
-    this.client.user.setPresence({ status: 'online', activity: activities[0] });
-  
-    let activity = 1;
-  
-
-    setInterval(() => {
-      activities[2] = { name: `p!help | ${ this.client.guilds.cache.size} guilds`, type: 'WATCHING' };
-      activities[3] = { name: `p!help | ${ this.client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} users`, type: 'WATCHING' }; 
-      if (activity > 3) activity = 0;
-      this.client.user.setActivity(activities[activity]);
-      activity++;
-    }, 35000);
-
-
 			logger.info(`✅ loaded: Bot Status `, { label: 'Status' })
 
       
