ac66ca9cd861d81cb20deddcb45c2b388092731b
peterhanania <72696414+peterhanania@users.noreply.github.com>
2022-01-28 21:35:18 +0200

    New discord invite

--- commands/utility/support.js
+++ commands/utility/support.js
@@ -20,7 +20,7 @@ module.exports = class extends Command {
       
       const embed = new Discord.MessageEmbed()
         .setColor(message.guild.me.displayHexColor)
-        .setDescription(`${language.support}(https://discord.gg/FqdH4sfKBg) ${message.client.emoji.success}`);
+        .setDescription(`${language.support}(https://discord.gg/duBwdCvCwW) ${message.client.emoji.success}`);
 
       await message.channel.send(embed)  
     }
